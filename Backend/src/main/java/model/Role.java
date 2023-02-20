9ba96bc5569dbe878638ed3c188ce8a71a9c4950
Kanishka-N <124807591+Kanishka-N@users.noreply.github.com>
2023-02-20 15:22:12 +0530

    new commit

--- Backend/src/main/java/model/Role.java
+++ Backend/src/main/java/model/Role.java
@@ -7,24 +7,29 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "role")
 public class Role {
     @Id
+    //Primary key as employee ID should be unique
     private String id;
     @Indexed(unique = true, direction = IndexDirection.DESCENDING)
 
+    //Role of employee
     private String role;
 
+    //getter for ID
     public String getId() {
         return id;
     }
 
+    //setter for ID
     public void setId(String id) {
         this.id = id;
     }
-    //hi
 
+    //getter for role
     public String getRole() {
         return role;
     }
 
+    //setter for role
     public void setRole(String role) {
         this.role = role;
     }
--- Backend/src/main/java/model/User.java
+++ Backend/src/main/java/model/User.java
@@ -12,8 +12,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
 
     @Id
+    //Primary key as employee ID should be unique
     private String id;
-    @Indexed(unique = true, direction = IndexDirection.DESCENDING)
+    @Indexed(unique = true)
     private String email;
     private String password;
     private String fullname;
@@ -21,34 +22,42 @@ public class User {
     @DBRef
     private Set<Role> roles;
 
+    //getter for ID
     public String getId() {
         return id;
     }
 
+    //setter for ID
     public void setId(String id) {
         this.id = id;
     }
 
+    //getter for email
     public String getEmail() {
         return email;
     }
 
+    //setter for email
     public void setEmail(String email) {
         this.email = email;
     }
 
+    //getter for password
     public String getPassword() {
         return password;
     }
 
+    //setter for password
     public void setPassword(String password) {
         this.password = password;
     }
 
+    //getter for fullname
     public String getFullname() {
         return fullname;
     }
 
+    //setter for fullname
     public void setFullname(String fullname) {
         this.fullname = fullname;
     }
@@ -57,14 +66,17 @@ public class User {
         return enabled;
     }
 
+    //setter for enabled
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
 
+    //getter for roles
     public Set<Role> getRoles() {
         return roles;
     }
 
+    //setter for roles
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
